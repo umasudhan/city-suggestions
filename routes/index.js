087aca3eaef43ad02617dc05299f14b8f168aa39
Uma Sudhan P <umasudhan.p@gmail.com>
2017-08-07 11:34:31 +1000

    This is the entry point which sets up all class dependencies. All dependencies are injected here to enable stubbing while writing tests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Suggester = require('../lib/suggester');
-const CityRepository = require('../lib/city-repositiory');
-const cityRepository = new CityRepository();
-const suggester = new Suggester(cityRepository);
+const CityRepository = require('../lib/city-repository');
+const MatchRanker = require('../lib/match-ranker');
+const cities = require('../data/cities_canada-usa');
+const cityRepository = new CityRepository(cities);
+const matchRanker = new MatchRanker();
+const suggester = new Suggester(cityRepository, matchRanker);
 
 router.get('/suggestions', suggester.getSuggestions );
 
